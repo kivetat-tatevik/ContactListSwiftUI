3a9428f2605c6ba37848606685e0b9640831765b
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-28 17:12:03 +0200

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Tatevik Khunoyan on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
